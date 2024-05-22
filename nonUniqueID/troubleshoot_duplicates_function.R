dfd19f525c9a93a0e3ef091f3e0d47c66f6320b3
D.PrBG.240 <efransen@login.biomina.be>
2024-05-22 14:40:16 +0200

    file with function only

--- nonUniqueID/troubleshoot_duplicates_function.R
+++ nonUniqueID/troubleshoot_duplicates_function.R
@@ -1,14 +1,4 @@
-setwd("C:/Users/fransen/Dropbox/Howest/11_Traineeship/practiceAnnovar/outputChr4.vcf")
-library(vcfR)
-library(tidyverse)
 
-# read in original VCF
-myVCF<-read.vcfR("annoChr4.hg19_multianno.vcf")
-
-# replace GERP++ by GERP in meta, fix and gt
-myVCF@meta<-gsub("GERP++_","GERP",myVCF@meta,fixed=TRUE)
-myVCF@fix<-gsub("GERP++_","GERP",myVCF@fix,fixed=TRUE)
-myVCF@gt<-gsub("GERP++_","GERP",myVCF@gt,fixed=TRUE)
 
 #### function to:
   ##  detect duplicate IDs
@@ -52,19 +42,4 @@ detect_dupl_ID<-function(inputVCF){
 }
 
 
-# functions failing due to non-unique ID
-tidyVCF<-vcfR2tidy(myVCF)
-genotypes_tidy<-extract_gt_tidy(myVCF)
-genotypes<-extract.gt(myVCF)
 
-
-# run function to correct
-myVCF<-detect_dupl_ID(inputVCF = myVCF)
-# [1] "ID >164>169 is duplicated"
-# [1] "Setting ID >164>169 to missing"
-
-
-# functions working after fixing duplicate IDs
-tidyVCF<-vcfR2tidy(myVCF)
-genotypes_tidy<-extract_gt_tidy(myVCF)
-genotypes<-extract.gt(myVCF)
