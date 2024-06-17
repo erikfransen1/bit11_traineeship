a0dbb19a3d315fc9ba3bc2ed952282cc6e02e95f
erikfransen1 <erik.fransen@student.howest.be>
2024-06-18 00:13:31 +0200

    Update splitVCF_wsl.R

--- splitVCF_wsl.R
+++ splitVCF_wsl.R
@@ -20,27 +20,43 @@ nMetaRows<-length(metapart)
 # read in meta part, headerline and body using read.table from entire VCF
 #to later do selection of rows in body VCF
 
-# meta part
-metapart<-read.table("NA12878.vcf",comment.char = "",nrows = nMetaRows,sep="\t")
+#### in bash ### 
+# split metapart + headerrow from body of the VCF
+head -n 49 NA12878.vcf > metaHeader
+tail -n +50 NA12878.vcf > bodyVCF
+$wc -l bodyVCF 
+#4167900 bodyVCF
+$ wc -l metaHeader 
+49 metaHeader
 
-# header row (between meta part and body)
-headerrow<-read.table("NA12878.vcf",comment.char = "",skip=nMetaRows,nrows = 1,sep="\t")
+### in R ###
+setwd("../ownVCF")
 
-# read in the non-metapart as dataframe (skipping meta lines and header line)
+# read in the body of the (entire) VCF as dataframe (skipping meta lines and header line)
 
-bodyVCF<-read.table("NA12878.vcf",skip=(1+nMetaRows),sep="\t")
+bodyVCF<-read.table("bodyVCF",sep="\t")
 nrow<-dim(bodyVCF)[1]
 
-names(headerrow)<-names(bodyVCF1)
-
 # compose first VCF
-# metapart = add to every VCF before annotation
-# headerline between metapart and fixed+info+gt
-# generate random 10,000 index numbers
-# split fixed part and GT with 
+# generate random 10,000 index numbers to pick rows from bodyVCF
 # 
+commands<-rep(NA,10)
+for(i in 1:10){
+    randomRows<-sample(1:nrow,10000,replace=FALSE)
+    bodySub<-bodyVCF[randomRows,]
+    write.table(bodySub,paste0("body",i),row.names = FALSE, col.names = FALSE, quote=FALSE,sep="\t")
+    commands[i]<-paste0("cat metaHeader body",i," > subVCF",i,"\n")
+}
+write.table(commands,"makeSubVCF",row.names=FALSE,col.names = FALSE,quote=FALSE)
 
-rows1<-sample(1:nrow,100000,replace=FALSE)
-bodyVCF1<-vcfAsDF[rows1,]
-test<-rbind(headerrow,bodyVCF1)
+#### bash ####
+chmod u+x makeSubVCF
+./makeSubVCF
 
+# folder now contains 10 subVCF files
+
+#check if subVCF can be read in
+mySubVCF<-read.vcfR("subVCF1")
+metapart<-mySubVCF@meta
+nMetaRows<-length(metapart)
+str(mySubVCF)
