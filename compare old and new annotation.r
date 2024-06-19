49a4331127bcff6a28c05fe50c8061e4b8e75cb4
erikfransen1 <erik.fransen@student.howest.be>
2024-06-20 01:35:10 +0200

    automate for >1 VCF with outer loop

--- compare old and new annotation.r
+++ compare old and new annotation.r
@@ -5,49 +5,65 @@
 getwd()
 [1] "/home/efransen/outputAnnovar"
 
-setwd("/home/efransen/outputAnnovar")
-list.files(getwd())
+setwd("/home/efransen")
+
 library(vcfR)
 library(tidyverse)
 
-#read in annotated files as tab-delimited (NOT csv!!)
-newVCF<-read.table("myNewanno.hg19_multianno.txt",header=T,sep="\t",fill=T,na.strings = ".")
-oldVCF<-read.table("myOldanno.hg19_multianno.txt",header=T,sep="\t",fill=T,na.strings = ".") 
-# nolint
+#old and new annotations are in 2 separate folders
+oldAnnot<-list.files(paste0(getwd(),"/annotHg19_old"))
+newAnnot<-list.files(paste0(getwd(),"/annotHg19_new"))
 
-# not all fields are same
-# search for common info fields
-commonCOLS<-intersect(names(newVCF),names(oldVCF))
-commonINFO<-commonCOLS[6:length(commonCOLS)]
-nFields<-length(commonINFO)
-hasAnnot<-rep(NA,nFields)
-outList<-vector("list",nFields)
-names(outList)<-commonINFO
+# list of lists to store different annotations
+outList<-vector("list",length(oldAnnot))
+names(outList)<-oldAnnot
 
-#loop over info common fields (within one subVCF)
-for(i in 1:nFields){
+# outer loop : over VCFs
+for(j in 1:length(oldAnnot)){
+    j<-1
+    #read in annotated files as tab-delimited (NOT csv!!)
+    newVCF<-read.table(paste0("annotHg19_new/",newAnnot[j]),header=T,sep="\t",fill=T,na.strings = ".")
+    oldVCF<-read.table(paste0("annotHg19_old/",oldAnnot[j]),header=T,sep="\t",fill=T,na.strings = ".")
+    
 
-    # grep common info fields in old and new annotation files
-    grepCol<-grep(commonINFO[i],names(newVCF))
-    tmpNew<-newVCF[is.na(newVCF$Chr)==FALSE,c(1:5,grepCol)]
-    names(tmpNew)[6]<-"new"
+    # not all fields are same
+    # search for common info fields
+    commonCOLS<-intersect(names(newVCF),names(oldVCF))
+    commonINFO<-commonCOLS[6:length(commonCOLS)]
+    nFields<-length(commonINFO)
+    hasAnnot<-rep(NA,nFields)
 
-    grepCol<-grep(commonINFO[i],names(oldVCF))
-    tmpOld<-oldVCF[is.na(oldVCF$Chr),c(1:5,grepCol)]
-    names(tmpOld)[6]<-"old"
+    #within outList (per VCF) generate inner list by common INFO field
+    outList[[j]]<-vector("list",nFields)
+    names(outList[[j]])<-commonINFO
 
-    # detect if there is any annotation in the VCF
-    if(sum(is.na(tmpOld$old), is.na(tmpNew$new))==(dim(tmpNew)[1]+dim(tmpOld)[1])){
-        hasAnnot[i]<-FALSE
-    }
-    else{
-        hasAnnot[i]<-TRUE
-        oldNew<-merge(tmpOld,tmpNew)
-        outList[[i]]<-oldNew[oldNew$old!=oldNew$new,]
+    #loop over info common fields (within one subVCF)
+    for(i in 1:nFields){
+
+        # grep common info fields in old and new annotation files
+        grepCol<-grep(commonINFO[i],names(newVCF))
+        tmpNew<-newVCF[is.na(newVCF$Chr)==FALSE,c(1:5,grepCol)]
+        names(tmpNew)[6]<-"new"
+
+        grepCol<-grep(commonINFO[i],names(oldVCF))
+        tmpOld<-oldVCF[is.na(oldVCF$Chr),c(1:5,grepCol)]
+        names(tmpOld)[6]<-"old"
+
+        # detect if there is any annotation in the VCF
+        if(sum(is.na(tmpOld$old), is.na(tmpNew$new))==(dim(tmpNew)[1]+dim(tmpOld)[1])){
+            hasAnnot[i]<-FALSE
+        }
+        else{
+            hasAnnot[i]<-TRUE
+            oldNew<-merge(tmpOld,tmpNew)
+            outList[[j]][[i]]<-oldNew[oldNew$old!=oldNew$new,]
+
+        }
 
     }
 
 }
+
 
 #next steps : only of there is annotation in at leas 1 of the annotations
 
