68ab29ea5ff214212ba9eb916e52bd4e98192adf
erikfransen1 <erik.fransen@student.howest.be>
2024-06-25 11:12:13 +0200

    troubleshoot for-loop

--- annotate subVCF.bat
+++ annotate subVCF.bat
@@ -13,14 +13,26 @@ pwd
 table_annovar.pl ownVCF/subVCF1 annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewanno -remove -protocol dbnsfp30a -operation f -nastring . -polish
 table_annovar.pl ownVCF/subVCF1 annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldanno -remove -protocol ljb26_all -operation f -nastring . -polish
 
-for file in subVCF1 subVCF2 subVCF3 subVCF4
+# run over all subVCFs
+myDir="/ownVCF/allsubVCF"
+
+for file in subVCF1 subVCF2 subVCF3  subVCF4  subVCF5  subVCF6  subVCF7  subVCF8  subVCF9
     do
-        table_annovar.pl ownVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewanno$file -remove -protocol dbnsfp30a -operation f -nastring . -polish
-        table_annovar.pl ownVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldanno$file -remove -protocol ljb26_all -operation f -nastring . -polish
+        # echo "$file"
+
+        table_annovar.pl ownVCF/allsubVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$file -remove -protocol dbnsfp30a -operation f -nastring . -polish
+        table_annovar.pl ownVCF/allsubVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$file -remove -protocol ljb26_all -operation f -nastring . -polish
     done    
+
+# trying to automate
+for file in "$myDir"/*
+    do 
+        echo "$file" 
+    done
 
 # generate folders to store old resp. new annotations
 mkdir annotHg19_old
 mkdir annotHg19_new
-mv outputAnnovar/myNewannosub*hg19* annotHg19_new/
-mv outputAnnovar/myOldannosub*hg19* annotHg19_old/
+mv outputAnnovar/myNewAnnosub*hg19* annotHg19_new/
+mv outputAnnovar/myOldAnnosub*hg19* annotHg19_old/
+
