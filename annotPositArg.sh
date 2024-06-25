ec2d6ed01eae3b63780a25f9f6c43a75173f92e3
erikfransen1 <erik.fransen@student.howest.be>
2024-06-25 11:39:20 +0200

    first commit

---
+++ annotPositArg.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# bash with positional arguments
+# ask for old and new annotation dbnsfp30a
+
+echo -n "Give the old annotation database :"
+read $oldannot
+echo -n "Give the new annotation database :"
+read $newannot
