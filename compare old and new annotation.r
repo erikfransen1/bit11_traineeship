dafa9f46d6f05578cb962c5cb0fbc4512254c272
erikfransen1 <erik.fransen@student.howest.be>
2024-06-27 16:59:44 +0200

    fixed merge error

--- compare old and new annotation.r
+++ compare old and new annotation.r
@@ -12,16 +12,19 @@ library(tidyverse)
 oldAnnot<-list.files(paste0(getwd(),"/annotHg19_old"))
 newAnnot<-list.files(paste0(getwd(),"/annotHg19_new"))
 
-# list of lists to store different annotations
-outList<-vector("list",length(oldAnnot))
-names(outList)<-oldAnnot
+# vector of sites with diff annotations in old vs new
+diffAnnot<-NULL
 
 # outer loop : over VCFs
 for(j in 1:length(oldAnnot)){
     #read in annotated files as tab-delimited (NOT csv!!)
     newVCF<-read.table(paste0("annotHg19_new/",newAnnot[j]),header=T,sep="\t",fill=T,na.strings = ".")
     oldVCF<-read.table(paste0("annotHg19_old/",oldAnnot[j]),header=T,sep="\t",fill=T,na.strings = ".")
-    
+   
+   # positive control
+   #in each subVCF, change the first (SIFT) annotation
+   # will be on different chr in each subVCF
+    newVCF[1,6]<-"testvalue"
 
     # not all fields are same
     # search for common info fields
@@ -34,32 +37,36 @@ for(j in 1:length(oldAnnot)){
     outList[[j]]<-vector("list",nFields)
     names(outList[[j]])<-commonINFO
 
+
     #loop over info common fields (within one subVCF)
     for(i in 1:nFields){
 
         # grep common info fields in old and new annotation files
         grepCol<-grep(commonINFO[i],names(newVCF))
-        tmpNew<-newVCF[is.na(newVCF$Chr)==FALSE,c(1:5,grepCol)]
+        tmpNew<-newVCF[,c(1:5,grepCol)]
+        tmpNew<-tmpNew %>% 
+            filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
         names(tmpNew)[6]<-"new"
+        tmpNew$new<-as.character(tmpNew$new)
 
         grepCol<-grep(commonINFO[i],names(oldVCF))
-        tmpOld<-oldVCF[is.na(oldVCF$Chr),c(1:5,grepCol)]
+        tmpOld<-oldVCF[,c(1:5,grepCol)]
+        tmpOld<-tmpOld %>% 
+            filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
         names(tmpOld)[6]<-"old"
+        tmpOld$old<-as.character(tmpOld$old)
 
-        # detect if there is any annotation in the VCF
-        if(sum(is.na(tmpOld$old), is.na(tmpNew$new))==(dim(tmpNew)[1]+dim(tmpOld)[1])){
-            hasAnnot[i]<-FALSE
+        oldNew<-merge(tmpOld,tmpNew,by=c("Chr","Start","Ref","Alt"))
+            oldNew<-oldNew %>% 
+                filter(!is.na(old)|!is.na(new))%>%
+                mutate(old=replace_na(old,""),
+                        new=replace_na(new,""))           
+        diffAnnot<-as.data.frame(rbind(diffAnnot,oldNew[oldNew$old!=oldNew$new,]))
         }
-        else{
-            hasAnnot[i]<-TRUE
-            oldNew<-merge(tmpOld,tmpNew)
-            outList[[j]][[i]]<-oldNew[oldNew$old!=oldNew$new,]
-
-        }
+        
 
     }
 
-}
 
 #next steps : only of there is annotation in at leas 1 of the annotations
 
