7fbaca2c024093c0670be5a2d237b8794989dd3c
erikfransen1 <erik.fransen@student.howest.be>
2024-06-27 22:13:36 +0200

    run over all files in subdir

--- annotate subVCF.bat
+++ annotate subVCF.bat
@@ -1,38 +1,37 @@
 ## annotate subVCF with various annotations
 
 # add folder with perl scripts from annovar to $PATH
-export PATH="/home/efransen/annovar:$PATH"
+# export PATH="/home/efransen/annovar:$PATH"
 
-pwd
 # /home/efransen
 # subVCF are in folder ownVCF
 # databases are in folder annovar/humandb
 
 
+# run over all subVCFs in folder allsubVCF using find function
+    # --> full path to file
+# extract filename using basename function
 # annotate using old and new SIFT/Polyphen
-table_annovar.pl ownVCF/subVCF1 annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewanno -remove -protocol dbnsfp30a -operation f -nastring . -polish
-table_annovar.pl ownVCF/subVCF1 annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldanno -remove -protocol ljb26_all -operation f -nastring . -polish
+# write annotated VCF to subfolder myNewAnno resp. myOldAnno 
 
-# run over all subVCFs
-myDir="/ownVCF/allsubVCF"
 
-for file in subVCF1 subVCF2 subVCF3  subVCF4  subVCF5  subVCF6  subVCF7  subVCF8  subVCF9
-    do
-        # echo "$file"
-
-        table_annovar.pl ownVCF/allsubVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$file -remove -protocol dbnsfp30a -operation f -nastring . -polish
-        table_annovar.pl ownVCF/allsubVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$file -remove -protocol ljb26_all -operation f -nastring . -polish
-    done    
-
-# trying to automate
-for file in "$myDir"/*
-    do 
-        echo "$file" 
+myDir=$(find /home/efransen/ownVCF/allsubVCF/ -type f -iname "*")
+ 
+for file in $myDir; do
+        echo "$file"
+        filename=basename $file
+        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$filename -remove -protocol dbnsfp30a -operation f -nastring . -polish
+        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$filename -remove -protocol ljb26_all -operation f -nastring . -polish
     done
 
+
 # generate folders to store old resp. new annotations
-mkdir annotHg19_old
-mkdir annotHg19_new
+# if directory does not exist yet (-p option)
+# move old and new annotation to separate folders, 
+# serving as input for R script to find differences
+
+mkdir -p annotHg19_old
+mkdir -p annotHg19_new
 mv outputAnnovar/myNewAnnosub*hg19* annotHg19_new/
 mv outputAnnovar/myOldAnnosub*hg19* annotHg19_old/
 
