490bb32730f43844d52264832ac04a3eb58756c6
erikfransen1 <erik.fransen@student.howest.be>
2024-06-28 11:38:55 +0200

    added -vcfinput argument to annovar command

--- annotPositArg.sh
+++ annotPositArg.sh
@@ -32,8 +32,8 @@ cd /home/efransen
 for file in $myDir; do
         echo "$file"
         filename=$(basename $file)
-        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$filename -remove -protocol $newannot -operation f -nastring . -polish
-        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$filename -remove -protocol $oldannot -operation f -nastring . -polish
+        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$filename -remove -protocol $newannot -operation f -nastring . -polish -vcfinput
+        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$filename -remove -protocol $oldannot -operation f -nastring . -polish -vcfinput
     done
 
 
