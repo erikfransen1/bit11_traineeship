a8bba758843d0071318e4b03579feed43d49891b
erikfransen1 <erik.fransen@student.howest.be>
2024-06-28 09:50:39 +0200

    new version of annot+folder as pos arg

--- annotPositArg.sh
+++ annotPositArg.sh
@@ -1,29 +1,51 @@
-# !/bin/bash
-# bash with positional arguments
-# ask for old and new annotation dbnsfp30a
-# needed dos2unix script !!!
+## annotate subVCF with various annotations
 
-echo "enter old db, new db and filename"
+# add folder with perl scripts from annovar to $PATH
+# export PATH="/home/efransen/annovar:$PATH"
+
+# /home/efransen
+# subVCF are in folder ownVCF
+# databases are in folder annovar/humandb
+
+
+# run over all subVCFs in folder allsubVCF using find function
+    # --> full path to file
+# extract filename using basename function
+# annotate using old and new SIFT/Polyphen
+# write annotated VCF to subfolder myNewAnno resp. myOldAnno 
+
+# user specifies which db to compare using positional arguments
+# new = dbnsfp30a
+# old = ljb26_all
+
+# !/bin/bash
 
  oldannot=$1
  newannot=$2
- file=$3
+ oldannotFolder=$3
+ newannotFolder=$4
 
+myDir=$(find /home/efransen/ownVCF/allsubVCF/ -type f -iname "*")
 
-echo "Comparing $oldannot to $newannot for $file"
+cd /home/efransen
+ 
+for file in $myDir; do
+        echo "$file"
+        filename=$(basename $file)
+        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$filename -remove -protocol $newannot -operation f -nastring . -polish
+        table_annovar.pl $file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$filename -remove -protocol $oldannot -operation f -nastring . -polish
+    done
 
-#carry out old and new annotation
-table_annovar.pl ownVCF/allsubVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myNewAnno$file -remove -protocol $newannot -operation f -nastring . -polish
-table_annovar.pl ownVCF/allsubVCF/$file annovar/humandb/ -buildver hg19 -out outputAnnovar/myOldAnno$file -remove -protocol $oldannot -operation f -nastring . -polish
-
-# dbnsfp30a 
-# ljb26_all 
-# subVCF1
 
 # generate folders to store old resp. new annotations
-#if these folders don't exit yet
-mkdir -p annotHg19_old
-mkdir -p annotHg19_new
-mv outputAnnovar/myNewAnnosub*hg19* annotHg19_new/
-mv outputAnnovar/myOldAnnosub*hg19* annotHg19_old/
+# if directory does not exist yet (-p option)
+# move old and new annotation to separate folders, 
+# serving as input for R script to find differences
+# outputfolder = part of positional argument
+
+mkdir -p $oldannotFolder
+mkdir -p $newannotFolder
+mv outputAnnovar/myOldAnnosub* $oldannotFolder/
+mv outputAnnovar/myNewAnnosub* $newannotFolder/
+
 
