64799c9da8bcb4ae8f3be46ba9ce09d522bc556b
erikfransen1 <erik.fransen@student.howest.be>
2024-06-29 22:04:59 +0200

    develop function to ggplot

--- compare old and new annotation.r
+++ compare old and new annotation.r
@@ -85,25 +85,32 @@ for(j in 1:length(oldAnnot)){
 allGenes<-unique(diffAnnot$Gene.refGene)
 # 3979 different genes with at leas 1 diff annotation
 
-perVCF<-diffAnnot%>%
-    filter(VCF=="subVCF1")%>%
-    ggplot(aes(x=field,fill=Func.refGene))+
-        geom_bar()+
-        xlab("")+
-        theme(axis.text.x = element_text(angle = 90))
+# function to draw graph from gene or VCF of interest
+plotDiffAnnot<-function(perVCF=FALSE,perGene=TRUE,geneOfInt="myGene",myVCF="myVCF",outputGraph="annotGraph.pdf"){
+    
+    if(perVCF==TRUE){
+        diffAnnot%>%
+        filter(VCF==myVCF)%>%
+        ggplot(aes(x=field,fill=Func.refGene))+
+            geom_bar()+
+            xlab("")+
+            theme(axis.text.x = element_text(angle = 90))
+    }
 
-geneOfInt<-"TTN"
+    if(perGene==TRUE){
+        selection<-diffAnnot%>%
+            filter(Gene.refGene==geneOfInt)
 
-selection<-diffAnnot%>%
-    filter(Gene.refGene==geneOfInt)
-
-pdf("output.pdf")
-if(dim(selection)[1]>0){
-    ggplot(selection,aes(x=old,y=new,col=Func.refGene))+
-        geom_point()+
-        ggtitle("Gene = ",geneOfInt)+
-        facet_wrap(~field,scales="free")
+        if(dim(selection)[1]>0){
+            ggplot(selection,aes(x=old,y=new,col=Func.refGene))+
+                geom_point()+
+                ggtitle("Gene = ",geneOfInt)+
+                facet_wrap(~field,scales="free")
+        }
+    }
 }
+pdf("annotTTN_2.pdf")
+plotDiffAnnot(perGene=TRUE,geneOfInt="TTN",outputGraph="annotTTN.pdf")
 dev.off()
 
 maxAnnot<-max(table(diffAnnot$Gene.refGene))
