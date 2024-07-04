312461fdf70864cc030655c32d6ff3eda38c7502
erikfransen1 <erik.fransen@student.howest.be>
2024-07-04 22:00:25 +0200

    Update compare old and new annotation.r

--- compare old and new annotation.r
+++ compare old and new annotation.r
@@ -76,43 +76,6 @@ for(j in 1:length(oldAnnot)){
         diffAnnot<-as.data.frame(rbind(diffAnnot,oldNew[oldNew$old!=oldNew$new,]))
 
         # table with differential annotations per VCF
-        outList[[j]][[i]]<-oldNew[oldNew$old!=oldNew$new,]
+        # outList[[j]][[i]]<-oldNew[oldNew$old!=oldNew$new,]
         }
     }
-
-
-# visualize differential expression
-allGenes<-unique(diffAnnot$Gene.refGene)
-# 3979 different genes with at leas 1 diff annotation
-
-# function to draw graph from gene or VCF of interest
-plotDiffAnnot<-function(perVCF=FALSE,perGene=TRUE,geneOfInt="myGene",myVCF="myVCF",outputGraph="annotGraph.pdf"){
-    
-    if(perVCF==TRUE){
-        diffAnnot%>%
-        filter(VCF==myVCF)%>%
-        ggplot(aes(x=field,fill=Func.refGene))+
-            geom_bar()+
-            xlab("")+
-            theme(axis.text.x = element_text(angle = 90))
-    }
-
-    if(perGene==TRUE){
-        selection<-diffAnnot%>%
-            filter(Gene.refGene==geneOfInt)
-
-        if(dim(selection)[1]>0){
-            ggplot(selection,aes(x=old,y=new,col=Func.refGene))+
-                geom_point()+
-                ggtitle("Gene = ",geneOfInt)+
-                facet_wrap(~field,scales="free")
-        }
-    }
-}
-pdf("annotTTN_2.pdf")
-plotDiffAnnot(perGene=TRUE,geneOfInt="TTN",outputGraph="annotTTN.pdf")
-dev.off()
-
-maxAnnot<-max(table(diffAnnot$Gene.refGene))
-table(diffAnnot$Gene.refGene)[table(diffAnnot$Gene.refGene)==maxAnnot]
-# TTN
