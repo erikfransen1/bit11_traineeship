262478ec79542a60f556e5103e118955ced355aa
erikfransen1 <erik.fransen@student.howest.be>
2024-07-05 22:07:08 +0200

    split numeric vs. character

--- visualizeDiffAnnot.r
+++ visualizeDiffAnnot.r
@@ -1,8 +1,10 @@
 
 # visualize differential annotation
 # load workspace from previous script
-load("~/diffAnnot.rda")
-# using diffAnnot df from previous R script
+load("~/diffAnnot_num.rda")
+load("~/diffAnnot_char.rda")
+# using diffAnnot df from previous R script,
+# with numeric and categorical in different files
 
 allGenes<-unique(diffAnnot$Gene.refGene)
 # 3979 different genes with at leas 1 diff annotation
@@ -10,8 +12,29 @@ allGenes<-unique(diffAnnot$Gene.refGene)
 tail(sort(table(diffAnnot$Gene.refGene)),10)
 
 # some annotations are continuous, some categorical
+unique(diffAnnot_num$field)
+# [1] "SIFT_score"             "Polyphen2_HDIV_score"   "Polyphen2_HVAR_score"
+# [4] "LRT_score"              "MutationTaster_score"   "MutationAssessor_score"
+# [7] "FATHMM_score"           "VEST3_score"            "CADD_raw"
+#[10] "CADD_phred"             "GERP.._RS"              "SiPhy_29way_logOdds"
+
+unique(diffAnnot_char$field)
+# [1] "SIFT_pred"             "Polyphen2_HDIV_pred"   "Polyphen2_HVAR_pred"
+# [4] "LRT_pred"              "MutationTaster_pred"   "MutationAssessor_pred"
+# [7] "FATHMM_pred"
+
+# see https://genome.ucsc.edu/cgi-bin/hgVai
+
 # most interesting = change to D(eleterious)
-table(diffAnnot[diffAnnot$field=="SIFT_pred",]$new,diffAnnot[diffAnnot$field=="SIFT_pred",]$old)
+# main diagonal is always empty !!! Has been selected against in previous step
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]$new,diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="Polyphen2_HDIV_pred",]$new,diffAnnot_char[diffAnnot_char$field=="Polyphen2_HDIV_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="Polyphen2_HVAR_pred",]$new,diffAnnot_char[diffAnnot_char$field=="Polyphen2_HVAR_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="LRT_pred",]$new,diffAnnot_char[diffAnnot_char$field=="LRT_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="MutationTaster_pred",]$new,diffAnnot_char[diffAnnot_char$field=="MutationTaster_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="MutationAssessor_pred",]$new,diffAnnot_char[diffAnnot_char$field=="MutationAssessor_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="FATHMM_pred",]$new,diffAnnot_char[diffAnnot_char$field=="FATHMM_pred",]$old,useNA="always"))
+
 
 
 myVCF<-"subVCF1"
