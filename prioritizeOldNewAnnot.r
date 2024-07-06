b4a6468c84fac21d9be0716eb7a564bb72fb4c7b
erikfransen1 <erik.fransen@student.howest.be>
2024-07-06 21:13:22 +0200

    split prioritizing and visualization

--- prioritizeOldNewAnnot.r
+++ prioritizeOldNewAnnot.r
@@ -1,88 +1,189 @@
-###compare old and new annotation
-# using input from bash script with positional arguments
 
-setwd("/home/efransen")
+# Prioritize differentially annotated variants
 
-library(vcfR)
-library(tidyverse)
+# load workspace from previous script
+load("~/diffAnnot_num.rda")
+load("~/diffAnnot_char.rda")
+# using diffAnnot df from previous R script,
+# with numeric and categorical in different files
 
-#old and new annotations are in 2 separate folders
-oldAnnot<-list.files(paste0(getwd(),"/oldAnnot3"))
-newAnnot<-list.files(paste0(getwd(),"/newAnnot3"))
+allGenes<-unique(diffAnnot$Gene.refGene)
+# 3979 different genes with at leas 1 diff annotation
+# gene with most differential annotations
+tail(sort(table(diffAnnot$Gene.refGene)),10)
 
-# need the txt files, not .vcf or .avinput
-whatfiles<-grep("\\.txt",oldAnnot)
-oldAnnot<-oldAnnot[whatfiles]
-whatfiles<-grep("\\.txt",newAnnot)
-newAnnot<-newAnnot[whatfiles]
+# some annotations are continuous, some categorical
+unique(diffAnnot_num$field)
+# [1] "SIFT_score"             "Polyphen2_HDIV_score"   "Polyphen2_HVAR_score"
+# [4] "LRT_score"              "MutationTaster_score"   "MutationAssessor_score"
+# [7] "FATHMM_score"           "VEST3_score"            "CADD_raw"
+#[10] "CADD_phred"             "GERP.._RS"              "SiPhy_29way_logOdds"
 
-# vector of sites with diff annotations in old vs new
-diffAnnot_char<-NULL
-diffAnnot_num<-NULL
-filename<-substr(oldAnnot,10,16)
+unique(diffAnnot_char$field)
+# [1] "SIFT_pred"             "Polyphen2_HDIV_pred"   "Polyphen2_HVAR_pred"
+# [4] "LRT_pred"              "MutationTaster_pred"   "MutationAssessor_pred"
+# [7] "FATHMM_pred"
+
+# character fields are more biologically relevant
+# most interesting = change to D(eleterious)
+# see https://genome.ucsc.edu/cgi-bin/hgVai
+# main diagonal is always empty !!! Has been selected against in previous step
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]$new,diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="Polyphen2_HDIV_pred",]$new,diffAnnot_char[diffAnnot_char$field=="Polyphen2_HDIV_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="Polyphen2_HVAR_pred",]$new,diffAnnot_char[diffAnnot_char$field=="Polyphen2_HVAR_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="LRT_pred",]$new,diffAnnot_char[diffAnnot_char$field=="LRT_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="MutationTaster_pred",]$new,diffAnnot_char[diffAnnot_char$field=="MutationTaster_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="MutationAssessor_pred",]$new,diffAnnot_char[diffAnnot_char$field=="MutationAssessor_pred",]$old,useNA="always"))
+addmargins(table(diffAnnot_char[diffAnnot_char$field=="FATHMM_pred",]$new,diffAnnot_char[diffAnnot_char$field=="FATHMM_pred",]$old,useNA="always"))
 
 
-# outer loop : over VCFs
-for(j in 1:length(oldAnnot)){
-    #read in annotated files as tab-delimited (NOT csv!!)
-    newVCF<-read.table(paste0("newAnnot3/",newAnnot[j]),header=T,sep="\t",fill=T,na.strings = ".")
-    oldVCF<-read.table(paste0("oldAnnot3/",oldAnnot[j]),header=T,sep="\t",fill=T,na.strings = ".")
-   
-    # not all fields are same
-    # search for common info fields
-    commonCOLS<-intersect(names(newVCF),names(oldVCF))
-    # remove columns with otherinfo
-    #only keep refGene annot on funct.refGene and Gene.refGene
-    commonCOLS<-commonCOLS[grepl("Otherinfo",commonCOLS)==FALSE]
-    commonINFO<-commonCOLS[11:length(commonCOLS)]
-    nFields<-length(commonINFO)
-    
+subSIFT<-diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]
 
-    #loop over info common fields (within one subVCF)
-    for(i in 1:nFields){
+interestingSIFT<-subSIFT%>%
+    filter(new=="D")%>%
+    filter(is.na(old)|old!="D")
+# SIFT : D(amaging) or T(olerated)
+# Polyphen : D(amaging); P(ossibly damaging), B(enign)
+# LRT_pred : D(eleterious), N(eutral), U(nknown)
+# MutationsTaster : A(utomatic), D(amaging), N(eutral),P(olymorfism)
+# MutationAssessor : H(igh), M(edium), L(ow), N(eutral)
+# FATHMM : D(eleterious), T(olerated)
 
-        # grep common info fields in old and new annotation files
-        grepCol<-grep(commonINFO[i],names(newVCF))
-        tmpNew<-newVCF[,c(1:7,grepCol)]
-        tmpNew<-tmpNew %>% 
-            filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
-        names(tmpNew)[8]<-"new"
+filterCharacter<-function(myField,strict=FALSE){
 
-        grepCol<-grep(commonINFO[i],names(oldVCF))
-        tmpOld<-oldVCF[,c(1:7,grepCol)]
-        tmpOld<-tmpOld %>% 
-            filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
-        names(tmpOld)[8]<-"old"
-
-        oldNew<-merge(tmpOld,tmpNew,by=c("Chr","Start","Ref","Alt","Func.refGene","Gene.refGene"))
-            oldNew<-oldNew %>% 
-                filter(!is.na(old)|!is.na(new))%>%
-                filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
-   
-        oldNew$field<-commonINFO[i]
-        oldNew$VCF<-filename[j]   
-        
-        # accumulate all rown with differential annotation
-        # split for numeric and character annotation
-        if(is.numeric(oldNew$new)){
-            diffAnnot_num<-as.data.frame(rbind(diffAnnot_num,oldNew[oldNew$old!=oldNew$new,]))
-            diffAnnot_num<-as.data.frame(rbind(diffAnnot_num,oldNew[is.na(oldNew$old)&is.na(oldNew$new)==FALSE,]))
-            diffAnnot_num<-as.data.frame(rbind(diffAnnot_num,oldNew[is.na(oldNew$new)&is.na(oldNew$old)==FALSE,]))
-        }else{
-            diffAnnot_char<-as.data.frame(rbind(diffAnnot_char,oldNew[oldNew$old!=oldNew$new,]))
-            diffAnnot_char<-as.data.frame(rbind(diffAnnot_char,oldNew[is.na(oldNew$old)&is.na(oldNew$new)==FALSE,]))
-            diffAnnot_char<-as.data.frame(rbind(diffAnnot_char,oldNew[is.na(oldNew$new)&is.na(oldNew$old)==FALSE,]))
-        }
-        
-        }
+    if (is.null(myField) || length(myField) != 1L || !myField %in% unique(diffAnnot_char$field)) {
+    stop("myField argument must be one of: 'SIFT_pred','Polyphen2_HDIV_pred','Polyphen2_HVAR_pred','LRT_pred',
+    'MutationTaster_pred','MutationAssessor_pred','FATHMM_pred'")
     }
 
-diffAnnot_num<-diffAnnot_num%>%
-    filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
+    tmpSubset<-diffAnnot_char[diffAnnot_char$field==myField,]
 
-diffAnnot_char<-diffAnnot_char%>%
-    filter(if_all(c(Chr,Start, Ref, Alt), ~!is.na(.)))
+    if(myField%in%c("SIFT_pred","LRT_pred","FATHMM_pred")){
+        topclass<-"D"
+    } else if (myField%in%c("Polyphen_HDIV_pred","Polyphen2_HVAR_pred")&strict==FALSE){
+        topclass<-c("D","P")
+     } else if (myField%in%c("Polyphen_HDIV_pred","Polyphen2_HVAR_pred")&strict==TRUE){
+        topclass<-c("D")
+    } else if (myField=="FATHMM_pred"&strict==FALSE){
+        topclass<-c("H","M")
+    } else if (myField=="FATHMM_pred"&strict==FALSE){
+        topclass<-c("H")
+    } else {
+        topclass<-c("A","D")
+    }
 
+    interesting<-tmpSubset%>%
+        filter(new%in%topclass)%>%
+        filter(is.na(old) | !old%in%topclass)
+    return(interesting)
+}
 
-save(diffAnnot_char,file="~/diffAnnot_char.rda")
-save(diffAnnot_num,file="~/diffAnnot_num.rda")
+# which field have uniquely numeric outcome?
+num_fields<-gsub("_score", "",unique(diffAnnot_num$field))
+char_fields<-gsub("_pred", "",unique(diffAnnot_char$field))
+setdiff(num_fields,char_fields)
+# [1] "VEST3"               "CADD_raw"            "CADD_phred"
+# [4] "GERP.._RS"           "SiPhy_29way_logOdds"
+
+# VEST3 and CADD_phred are most relevant to probability to be disease causing
+# GERP and SiPhy= evolutionalry conservation
+
+# filter out variant with biologically relevant change in annotation
+
+# VEST3 = p-value
+subVEST3<-diffAnnot_num[diffAnnot_num$field=="VEST3_score",]
+table(subVEST3$old<0.05)
+table(subVEST3$new<0.05)
+
+table(subVEST3$old<0.05,subVEST3$new<0.05,useNA="always")
+
+interestingVEST<-subVEST3%>%
+    filter(new<0.05)%>%
+    filter(is.na(old)|old>0.05)
+
+# CADD_raw = the higher, the more likely to be deleterious
+# but cannot be used to compare across CADD versions
+# https://cadd.gs.washington.edu/info
+# use CADD_phred instead for cutoff of deleteriousness
+subCADD_raw<-diffAnnot_num[diffAnnot_num$field=="CADD_raw",]
+subCADD_phred<-diffAnnot_num[diffAnnot_num$field=="CADD_phred",]
+
+# if CADD_score >10 : 10% most deleterious in human genome
+# if CADD_score >20 : 1% most deleterious in human genome
+table(subCADD_phred$old>10)
+table(subCADD_phred$new>10)
+
+table(subCADD_phred$old>20,subCADD_phred$new>20,useNA="always")
+
+# GERP : measure for evolutionary constring
+# https://genome.ucsc.edu/cgi-bin/hgTrackUi?db=hg19&g=allHg19RS_BW
+# RS score threshold of 2 provides high sensitivity while still strongly enriching for truly constrained sites.
+
+subGERP<-diffAnnot_num[diffAnnot_num$field=="GERP.._RS",]
+table(subGERP$old>2)
+table(subGERP$new>2)
+table(subGERP$old>2,subGERP$new>2,useNA="always")
+interestingGERP<-subGERP%>%
+    filter(new>2)%>%
+    filter(is.na(old)|old<2)
+
+subSiPhy<-diffAnnot_num[diffAnnot_num$field=="SiPhy_29way_logOdds",]
+quantile(subSiPhy$new,seq(0,1,0.1),na.rm=T)
+#     0%    10%    20%    30%    40%    50%    60%    70%    80%    90%   100% 
+#  0.182  2.277  4.034  4.584  5.615  7.206  8.217  9.489 11.936 14.232 20.474
+
+interestingSiPhy<-subSiPhy%>%
+    filter(new>14)%>%
+    filter(is.na(old)|old<14)
+
+# no cutoff found in literature
+#take 90th percentile
+
+#function to select variants with relevant change in annotation
+filterNumeric<-function(myField,myCutoff){
+
+    if (is.null(myField) || length(myField) != 1L || !myField %in% c("CADD_phred", "VEST3_score","GERP.._RS","SiPhy_29way_logOdds")) {
+    stop("myField argument must be one of: 'CADD_phred','VEST3_score','GERP.._RS','SiPhy_29way_logOdds'")
+    }
+
+    tmpSubset<-diffAnnot_num[diffAnnot_num$field==myField,]
+    interesting<-tmpSubset%>%
+        filter(new>myCutoff)%>%
+        filter(is.na(old)|old<myCutoff)
+    return(interesting)
+}
+
+interestCADD<-filterNumeric(myField = "CADD_phred",myCutoff=20)
+
+myVCF<-"subVCF1"
+
+# function to draw graph from gene or VCF of interest
+plotDiffAnnot<-function(perVCF=FALSE,perGene=FALSE,geneOfInt="myGene",myVCF="myVCF",outputGraph="annotGraph.pdf"){
+    
+    if(perVCF==TRUE){
+        diffAnnot%>%
+        filter(VCF==myVCF)%>%
+        ggplot(aes(x=field,fill=Func.refGene))+
+            geom_bar()+
+            xlab("")+
+            theme(axis.text.x = element_text(angle = 90))
+    }
+
+    if(perGene==TRUE){
+        selection<-diffAnnot%>%
+            filter(Gene.refGene==geneOfInt)
+
+        if(dim(selection)[1]>0){
+            ggplot(selection,aes(x=old,y=new,col=Func.refGene))+
+                geom_point()+
+                ggtitle("Gene = ",geneOfInt)+
+                facet_wrap(~field,scales="free")
+        }
+    }
+}
+
+plotDiffAnnot(perVCF=TRUE,myVCF="subVCF1",outputGraph="annotTTN.pdf")
+
+pdf("annotTTN_2.pdf")
+plotDiffAnnot(perGene=TRUE,geneOfInt="TTN",outputGraph="annotTTN.pdf")
+dev.off()
