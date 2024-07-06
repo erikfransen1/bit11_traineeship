c15f49a54cc03dd339c58a7a0d45973677edcbe7
erikfransen1 <erik.fransen@student.howest.be>
2024-07-06 14:48:18 +0200

    function for numeric cutoffs

--- visualizeDiffAnnot.r
+++ visualizeDiffAnnot.r
@@ -23,9 +23,9 @@ unique(diffAnnot_char$field)
 # [4] "LRT_pred"              "MutationTaster_pred"   "MutationAssessor_pred"
 # [7] "FATHMM_pred"
 
-# see https://genome.ucsc.edu/cgi-bin/hgVai
-
+# character fields are more biologically relevant
 # most interesting = change to D(eleterious)
+# see https://genome.ucsc.edu/cgi-bin/hgVai
 # main diagonal is always empty !!! Has been selected against in previous step
 addmargins(table(diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]$new,diffAnnot_char[diffAnnot_char$field=="SIFT_pred",]$old,useNA="always"))
 addmargins(table(diffAnnot_char[diffAnnot_char$field=="Polyphen2_HDIV_pred",]$new,diffAnnot_char[diffAnnot_char$field=="Polyphen2_HDIV_pred",]$old,useNA="always"))
@@ -36,6 +36,62 @@ addmargins(table(diffAnnot_char[diffAnnot_char$field=="MutationAssessor_pred",]$
 addmargins(table(diffAnnot_char[diffAnnot_char$field=="FATHMM_pred",]$new,diffAnnot_char[diffAnnot_char$field=="FATHMM_pred",]$old,useNA="always"))
 
 
+
+# which field have uniquely numeric outcome?
+num_fields<-gsub("_score", "",unique(diffAnnot_num$field))
+char_fields<-gsub("_pred", "",unique(diffAnnot_char$field))
+setdiff(num_fields,char_fields)
+# [1] "VEST3"               "CADD_raw"            "CADD_phred"
+# [4] "GERP.._RS"           "SiPhy_29way_logOdds"
+
+# VEST3 and CADD_phred are most relevant to probability to be disease causing
+# GERP and SiPhy= evolutionalry conservation
+
+# filter out variant with biologically relevant change in annotation
+
+# VEST3 = p-value
+subVEST3<-diffAnnot_num[diffAnnot_num$field=="VEST3_score",]
+table(subVEST3$old<0.05)
+table(subVEST3$new<0.05)
+
+table(subVEST3$old<0.05,subVEST3$new<0.05,useNA="always")
+
+interestingVEST<-subVEST3%>%
+    filter(new<0.05)%>%
+    filter(is.na(old)|old>0.05)
+
+# CADD_raw = the higher, the more likely to be deleterious
+# but cannot be used to compare across CADD versions
+# https://cadd.gs.washington.edu/info
+# use CADD_phred instead for cutoff of deleteriousness
+subCADD_raw<-diffAnnot_num[diffAnnot_num$field=="CADD_raw",]
+subCADD_phred<-diffAnnot_num[diffAnnot_num$field=="CADD_phred",]
+
+# if CADD_score >10 : 10% most deleterious in human genome
+# if CADD_score >20 : 1% most deleterious in human genome
+table(subCADD_phred$old>10)
+table(subCADD_phred$new>10)
+
+table(subCADD_phred$old>20,subCADD_phred$new>20,useNA="always")
+
+interestingCADD<-subCADD_phred%>%
+    filter(new>20)%>%
+    filter(is.na(old)|old<20)
+
+filterInterest<-function(myField,myCutoff){
+
+    if (is.null(myField) || length(myField) != 1L || !myField %in% c("CADD_phred", "VEST3_score")) {
+    stop("myField argument must be one of: 'CADD_phred','VEST3_score'")
+    }
+
+    tmpSubset<-diffAnnot_num[diffAnnot_num$field==myField,]
+    interesting<-tmpSubset%>%
+        filter(new>myCutoff)%>%
+        filter(is.na(old)|old<myCutoff)
+    return(interesting)
+}
+
+interestCADD<-filterInterest(myField = "CADD_phred",myCutoff=20)
 
 myVCF<-"subVCF1"
 
